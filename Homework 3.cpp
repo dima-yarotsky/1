40f46ff12587ce8586d9521d9e087b7cedebc0c1
Dmitry Yarotsky <31436662+dima-yarotsky@users.noreply.github.com>
2020-12-01 11:47:54 +0600

    Add files via upload

---
+++ Homework 3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include<locale.h>
+using namespace std;
+int main()
+{
+    char s[2000];
+    int a=0 , c = 0;
+    setlocale(LC_ALL, "Russian");
+    cout << "Количество слов в строке " << endl;
+    cin.get(s, 2000);
+for(int i=0;i<strlen(s);i++) 
+    {
+    if (s[i] == ' ' )
+        if (a == 0)continue;
+        else a = 0, c++;
+    else
+        a++;
+    }
+    cout << "Количество слов в строке " << c;
+    cin.get(); cin.get();
+    return 0;
+}
